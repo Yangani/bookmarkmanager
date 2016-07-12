9f706606b28ae5bcf9532db964030cf0d2991331
Festus Kiprop <fyangani@gmail.com>
2016-07-12 00:24:20 -0500

    Setting port

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var router = require('./router');
+var port = process.env.PORT || 3000;
 
 // express will look in this folder for static content 
 app.use(express.static('public'));
 
 app.use('/api', router);
 
-app.listen(3000, function() {
+app.listen(port, function() {
 	console.log("Server is running at port 3000");
 });
